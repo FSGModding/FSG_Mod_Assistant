c5f85774d18078d4fd7e3b788f5ba0a60120620c
J.T. Sage <jtsage+datebox@gmail.com>
2022-10-20 18:32:05 -0400

    Bug: new zip library doesn't file-not-found fail, so do it manually.  Add: debug dump for parser and a little tester to use it.

---
+++ test/single_read_test.js
@@ -0,0 +1,35 @@
+/*  _______           __ _______               __         __   
+   |   |   |.-----.--|  |   _   |.-----.-----.|__|.-----.|  |_ 
+   |       ||  _  |  _  |       ||__ --|__ --||  ||__ --||   _|
+   |__|_|__||_____|_____|___|___||_____|_____||__||_____||____|
+   (c) 2022-present FSG Modding.  MIT License. */
+
+// Test Program
+
+const { mcLogger }             = require('../lib/logger.js')
+const { modFileChecker }       = require('../lib/single-mod-checker.js')
+
+const logger = new mcLogger()
+
+console.log('FSG Mod Assistant : Test Mod Reader')
+console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n')
+
+if ( process.argv.length < 3 ) {
+	console.log('File not provided')
+	process.exit(1)
+}
+const thisModFile = process.argv[2]
+
+console.log(` --Testing: ${thisModFile}`)
+
+const thisMod = new modFileChecker(
+	thisModFile,
+	false,
+	0,
+	new Date(1970, 1, 1, 0, 0, 0, 0),
+	logger,
+	() => { return 'en'}
+)
+
+console.log(thisMod.debugDump)
+
