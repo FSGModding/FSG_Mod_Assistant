4b5d4a4e42150bde9c564476a9e355b71fdc6472
J.T. Sage <jtsage+datebox@gmail.com>
2023-01-13 15:57:15 -0500

    Test workflow 1

---
+++ test/outdated-deps.js
@@ -0,0 +1,22 @@
+const { exec } = require('node:child_process')
+
+exec('npm outdated --save false', (error, stdout) => {
+	if (error && stdout.length === 0) {
+		console.error(`exec error: ${error}`)
+		return
+	}
+	if ( stdout.length === 0 ) {
+		process.exit(1)
+	}
+	console.log('## Outdated Dependencies\n')
+	let firstLine = true
+	stdout.split('\n').forEach((line) => {
+		if ( line.length !== 0 ) {
+			console.log(`|${line.replaceAll(/\s\s+/g, '|')}|`)
+			if ( firstLine ) {
+				console.log('|---|---|---|---|---|---|')
+				firstLine = false
+			}
+		}
+	})
+})
