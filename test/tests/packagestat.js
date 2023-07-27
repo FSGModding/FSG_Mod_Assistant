9f9a53f1f7ab8fea766eba2545e52dd02b6d7e3c
J.T. Sage <jtsage+datebox@gmail.com>
2023-07-27 14:29:50 -0400

    Re-write statistics package

--- test/tests/packagestat.js
+++ test/tests/packagestat.js
@@ -7,9 +7,11 @@
 // Test Program - Language Files
 
 const path      = require('path')
+const fs        = require('fs')
 const {glob}    = require('glob')
 const {testLib} = require('../test.js')
-const imgSet    = new Set(['.jpg', '.png', '.psd', '.ico'])
+const imgSet    = new Set(['.jpg', '.png', '.psd', '.ico', '.icns', '.woff', '.woff2', '.webp', '.zip'])
+const knowSet   = new Set([...imgSet, '.css', '.scss', '.xml', '.js', '.json', '.html', '.md'])
 
 module.exports.test = async () => {
 	return Promise.all([
@@ -17,31 +19,79 @@ module.exports.test = async () => {
 	])
 }
 
-const getSize = (arr) => {
-	const realTotal = arr.map((x) => x.size).reduce((total, x) => total + x, 0)
+const countOthers = (arr) => {
+	return {
+		blank : 0,
+		code : 0,
+		comment : 0,
+		size : arr.map((x) => x.size).reduce((total, x) => total + x, 0),
+		total : arr.length,
+	}
+}
+
+const countTextFile = (fileList, isJS = false) => {
+	return fileList.reduce((total, x) => {
+		const fileContents = fs.readFileSync(x.fullpath(), 'utf-8').replace(/\r/g, '')
+
+		let   thisComment = (fileContents.match(/^[ |\t]*\/\/.+\r*$/gm) ?? []).length
+		const thisBlank   = (fileContents.match(/^[ \t]*\r*$/gm) || []).length
+		const thisTotal   = fileContents.split('\n').length
+
+		if ( isJS ) {
+			const matches = fileContents.match(/(?<=\n|^|\n\s)\/\*.+?\*\//gs)
+			if ( matches ) {
+				for ( const thisMatch of matches ) {
+					for ( const thisCommentLine of thisMatch.split('\n') ) {
+						if ( thisCommentLine !== '' ) {
+							thisComment++
+						}
+					}
+				}
+			}
+		}
+
+		total.code    += (thisTotal - thisComment - thisBlank)
+		total.comment += thisComment
+		total.blank   += thisBlank
+		total.size    += x.size
+		total.total++
+		return total
+	}, { blank : 0, code : 0, comment : 0, size : 0, total : 0 })
+}
+
+const padData   = (data, pad = 0) => data === 0 ? ''.padStart(pad) : data.toString().padStart(pad)
+
+const toNum     = (num) => typeof num !== 'number' || num === 0 ? num : Intl.NumberFormat('en').format(num)
+const toMByte   = (num) => typeof num !== 'number' ? num : Intl.NumberFormat('en', {maximumFractionDigits : 2, minimumFractionDigits : 2}).format(num / 1024 / 1024)
+
+const makeLine  = (type, data) => {
 	return [
-		'(',
-		Intl.NumberFormat('en').format(realTotal).padStart(11),
-		' bytes) [',
-		Intl.NumberFormat('en', {maximumFractionDigits : 2, minimumFractionDigits : 2}).format(realTotal / 1024 / 1024).padStart(5),
-		' Mb]'
+		type.padEnd(11), ' |',
+		padData(toNum(data.total), 7), ' |',
+		padData(toNum(data.blank), 7), ' |',
+		padData(toNum(data.code), 7), ' |',
+		padData(toNum(data.comment), 7), ' |',
+		padData(toNum(data.size), 12), ' |',
+		padData(toMByte(data.size), 6), ' MB |'
 	].join('')
 }
 
 const countFiles = (test) => {
 	return glob('**', { cwd : path.join(__dirname, '..', '..'), ignore : 'node_modules/**', stat : true, withFileTypes : true }).then((results) => {
 		const fileList = results.filter((x) => x.isFile())
-
-		const jsFiles   = fileList.filter((x) => path.extname(x.name) === '.js')
-		const htmlFiles = fileList.filter((x) => path.extname(x.name) === '.html')
-		const jsonFiles = fileList.filter((x) => path.extname(x.name) === '.json')
-		const imgFiles  = fileList.filter((x) => imgSet.has(path.extname(x.name)) )
-
-		test.step(`Total Files       : ${fileList.length.toString().padEnd(4)} ${getSize(fileList)}`)
-		test.step(` Image Files      : ${imgFiles.length.toString().padEnd(4)} ${getSize(imgFiles)}`)
-		test.step(` HTML Files       : ${htmlFiles.length.toString().padEnd(4)} ${getSize(htmlFiles)}`)
-		test.step(` JSON Files       : ${jsonFiles.length.toString().padEnd(4)} ${getSize(jsonFiles)}`)
-		test.step(` JavaScript Files : ${jsFiles.length.toString().padEnd(4)} ${getSize(jsFiles)}`)
+		test.step_fmt(makeLine('', { blank : 'Blank', code : 'Code', comment : '/* */', size : 'Size', total : 'Files' }))
+		test.step_fmt(''.padStart(74, '-'))
+		test.step_fmt(makeLine('javaScript', countTextFile(fileList.filter((x) => path.extname(x.name) === '.js'), true)))
+		test.step_fmt(makeLine('JSON', countTextFile(fileList.filter((x) => path.extname(x.name) === '.json'))))
+		test.step_fmt(makeLine('XML', countTextFile(fileList.filter((x) => path.extname(x.name) === '.xml'))))
+		test.step_fmt(makeLine('HTML', countTextFile(fileList.filter((x) => path.extname(x.name) === '.html'))))
+		test.step_fmt(makeLine('CSS', countTextFile(fileList.filter((x) => path.extname(x.name) === '.css' || path.extname(x.name) === '.scss'), true)))
+		test.step_fmt(makeLine('markDown', countTextFile(fileList.filter((x) => path.extname(x.name) === '.md'))))
+		test.step_fmt(makeLine('Images', countOthers(fileList.filter((x) => imgSet.has(path.extname(x.name)) ))))
+		test.step_fmt(makeLine('ZIP Archive', countOthers(fileList.filter((x) => path.extname(x.name) === '.md'))))
+		test.step_fmt(makeLine('Other', countOthers(fileList.filter((x) => !knowSet.has(path.extname(x.name)) ))))
+		test.step_fmt(''.padStart(74, '-'))
+		test.step_fmt(makeLine('TOTAL', countOthers(fileList)))
 	}).catch((err) => {
 		test.error(err)
 	}).finally(() => {
