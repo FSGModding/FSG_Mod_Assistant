2e000b523bd9ca2f157ca2245d597bbf4db759e1
J.T. Sage <jtsage+datebox@gmail.com>
2024-02-09 15:19:59 -0500

    Finish out automated HTML testing, correct or properly ignore all errors

--- test/tests/sourcehtml.js
+++ test/tests/sourcehtml.js
@@ -6,10 +6,11 @@
 
 // Test Program - ESLint Source Code
 
-const path         = require('node:path')
-const { globSync } = require('glob')
-// const { ESLint }   = require('eslint')
-const { testLib }  = require('../test.js')
+const path             = require('node:path')
+const { globSync }     = require('glob')
+const fs               = require('node:fs')
+const crypto           = require('node:crypto')
+const { testLib }      = require('../test.js')
 const { HtmlValidate } = require('html-validate')
 
 
@@ -24,30 +25,35 @@ async function tester (test) {
 
 	const thisPath  = path.join(__dirname, '..', '..', 'renderer', '**', '*.html').split(path.sep).join('/')
 	const htmlFiles = globSync(thisPath, { absolute : true })
+	let   errorTotal = 0
+	let   errorFiles = 0
 
 	const thisValidator = new HtmlValidate({
 		extends : ['html-validate:recommended'],
 		rules   : {
 			'element-name'                : ['error', { whitelist : ['l10n'] }],
+			'element-required-ancestor'   : 'off',
 			'element-required-attributes' : 'off',
 			'empty-heading'               : 'off',
 			'no-inline-style'             : 'off',
-
-			// 'doctype-style' : 'off',
-			// 'attr-quotes' : 'off',
-			'no-trailing-whitespace' : 'off',
-			'text-content' : 'off',
-			// 'void-style' : 'warn',
+			'no-trailing-whitespace'      : 'off',
+			'prefer-native-element'       : ['error', { exclude : ['progressbar']}],
+			'prefer-tbody'                : 'off',
+			'text-content'                : 'off',
+			'valid-id'                    : ['error', {'relaxed' : true}],
 		},
 	})
 
 	for ( const file of htmlFiles ) {
 		const thisFile = path.relative(rootPath, file)
-		
-		promiseArray.push(thisValidator.validateFile(file).then((result) => {
+		const fileContents = fs.readFileSync(file, { encoding : 'utf8' }).replace(/{{uuid}}/g, () => crypto.randomUUID())
+
+		promiseArray.push(thisValidator.validateString(fileContents).then((result) => {
 			if ( result.errorCount === 0 ) {
 				test.step(`${thisFile} is clean`)
 			} else {
+				errorFiles++
+				errorTotal += result.errorCount
 				test.error(`${thisFile} has ${result.errorCount} Errors`)
 				for ( const thisError of result.results[0].messages ) {
 					test.step_log(`${thisFile} :: [${thisError.line}] {${thisError.ruleId}} ${thisError.message}`)
@@ -56,5 +62,7 @@ async function tester (test) {
 			}
 		}))
 	}
-	return Promise.all(promiseArray)
+	return Promise.all(promiseArray).then(() => {
+		if ( errorTotal > 0 ) { test.error(`Total Errors Found : ${errorTotal} in ${errorFiles} files`)}
+	})
 }
