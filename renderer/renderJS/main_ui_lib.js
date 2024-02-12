0c6a8c75bd333d6e28cffa73e4b58da9b4217e2b
J.T. Sage <jtsage+datebox@gmail.com>
2024-02-11 22:13:54 -0500

    Step 2/2 - detect possibly malicious code

--- renderer/renderJS/main_ui_lib.js
+++ renderer/renderJS/main_ui_lib.js
@@ -127,6 +127,8 @@ const mainLib = {
 		
 		if ( Array.isArray(thisMod.displayBadges ) ) {
 			for ( const badge of thisMod.displayBadges ) {
+				if ( badge[0] === 'malware' && mainState.modCollect.appSettings.suppress_malware.includes(thisMod.colUUID)) { continue }
+
 				displayBadges.push(fsgUtil.badge_main(badge))
 				mainState.add_searchTagMap(badge[0], thisMod.colUUID)
 			}
