f8363c02d4b5490ce95b45a5c3d9d8f6462939d0
J.T. Sage <jtsage+datebox@gmail.com>
2024-03-23 13:15:46 -0400

    Missed a few.

--- renderer/renderJS/util/fillType.js
+++ renderer/renderJS/util/fillType.js
@@ -10,14 +10,14 @@
 
 /* cSpell:disable */
 /* eslint-disable comma-spacing, quotes */
-const ft_known = new Set(["cat-attach-has","cat-attach-need","cat-brand","cat-object","cat-placeable","cat-tool","cat-vehicle","fill-air","fill-alfalfa","fill-alfalfa-windrow","fill-anhydrous","fill-armoire","fill-bakedpoppy","fill-balenet","fill-baletwine","fill-barley","fill-barrel","fill-bathtub","fill-beanstraw","fill-beer","fill-birdhouse","fill-boards","fill-bowl","fill-bread","fill-bucket","fill-bun","fill-butter","fill-cake","fill-canola","fill-carp","fill-carrot","fill-carrotjuice","fill-carrotsalad","fill-cartonroll","fill-cattree","fill-cereals","fill-chaff","fill-chair","fill-cheese","fill-chicken","fill-chickenfeed","fill-chips","fill-chocolate","fill-chocolateicecream","fill-clothes","fill-clover","fill-clover-windrow","fill-coleslaw","fill-compost","fill-cornstalks","fill-cotton","fill-cow","fill-cream","fill-def","fill-diesel","fill-digestate","fill-dog","fill-doghouse","fill-dryalfalfa-windrow","fill-dryclover-windrow","fill-drygrass-windrow","fill-drymaize2","fill-duck","fill-easel","fill-egg","fill-electriccharge","fill-emptypallet","fill-ethanol","fill-fabric","fill-fertilizer","fill-fishfeed","fill-fishflour","fill-floortiles","fill-flour","fill-forage","fill-forage-mixing","fill-frenchfries","fill-furniture","fill-goat","fill-goatcheese","fill-goatmilk","fill-grape","fill-grapejuice","fill-grass","fill-grass-windrow","fill-greensalad","fill-haypellets","fill-herbicide","fill-honey","fill-horse","fill-ironore","fill-lettuce","fill-lime","fill-liquidfertilizer","fill-liquidmanure","fill-maize","fill-manure","fill-mashedpotato","fill-metal","fill-methane","fill-milk","fill-mineralfeed","fill-mohn","fill-molasses","fill-oat","fill-oatdrink","fill-oatmeal","fill-oatmilk","fill-oilradish","fill-oil-canola","fill-oil-olive","fill-oil-sunflower","fill-olive","fill-onion","fill-onionjuice","fill-paperroll","fill-parsnip","fill-pasta","fill-peppergrinder","fill-pickle-carrot","fill-pickle-parsnip","fill-pickle-redbeet","fill-pictureframe","fill-pig","fill-pigfood","fill-pike","fill-pizza","fill-plankslong","fill-pomace","fill-poplar","fill-poppyseedbun","fill-potato","fill-potatopancake","fill-potatosalad","fill-praline","fill-prefabwall","fill-pressurewasheradditive","fill-product","fill-propane","fill-raisins","fill-redbeet","fill-redcabbage","fill-roadsalt","fill-rockpowder","fill-roundbalecotton","fill-roundbalegrass","fill-roundbalehay","fill-roundbalesilage","fill-roundbalestraw","fill-roundbalewood","fill-rye","fill-salmon","fill-salt","fill-seeds","fill-seedtreatingliquid","fill-sheep","fill-shingle","fill-silage","fill-silageadditive","fill-snow","fill-sodiumchloride","fill-sorghum","fill-soup-carrot","fill-soup-parsnip","fill-soup-potato","fill-soup-redbeet","fill-soup-triple","fill-soybean","fill-soydrink","fill-soyflour","fill-soymilk","fill-spelt","fill-squarebalecotton","fill-squarebalegrass","fill-squarebalehay","fill-squarebalesilage","fill-squarebalestraw","fill-squarebalewood","fill-staircaserailing","fill-stone","fill-straw","fill-strawberry","fill-strawberrycreamcake","fill-strawberryicecream","fill-strawpellets","fill-strwberryjuice","fill-sugar","fill-sugarbeet","fill-sugarbeetcut","fill-sugarcane","fill-sunflower","fill-table","fill-tarp","fill-tomato","fill-tomatojuice","fill-tomatosalad","fill-unknown","fill-vinegar","fill-vitamins","fill-water","fill-weed","fill-wheat","fill-wheatsemolina","fill-whiskey","fill-whitecabbage","fill-wine","fill-wood","fill-woodbeam","fill-woodchips","fill-wool","look-beacons","look-diesel","look-electric","look-engine","look-fillunit","look-income","look-info","look-key","look-lights","look-methane","look-objects","look-paintable","look-price","look-prod-cycle","look-prod-input","look-prod-output","look-speed","look-speedlimit","look-timer","look-transmission","look-weight","look-wheels","look-width","look-year","ma-large","ma-small","season-fall","season-spring","season-summer","season-winter","sort-down","sort-none","sort-up","ver-13","ver-15","ver-17","ver-19","ver-22","weather-1","weather-2","weather-3","weather-4","weather-5","weather-6","weather-7","weather-8","weather-9"])
+const ft_known = new Set(["cat-attach-has","cat-attach-need","cat-brand","cat-object","cat-placeable","cat-tool","cat-vehicle","fill-air","fill-alfalfa","fill-alfalfa-windrow","fill-anhydrous","fill-armoire","fill-bakedpoppy","fill-balenet","fill-baletwine","fill-barley","fill-barrel","fill-bathtub","fill-beanstraw","fill-beer","fill-birdhouse","fill-boards","fill-bowl","fill-bread","fill-bucket","fill-bun","fill-butter","fill-cake","fill-canola","fill-carp","fill-carrot","fill-carrotjuice","fill-carrotsalad","fill-cartonroll","fill-cattree","fill-cereals","fill-chaff","fill-chair","fill-cheese","fill-chicken","fill-chickenfeed","fill-chips","fill-chocolate","fill-chocolateicecream","fill-clothes","fill-clover","fill-clover-windrow","fill-coleslaw","fill-compost","fill-cornstalks","fill-cotton","fill-cow","fill-cream","fill-def","fill-diesel","fill-digestate","fill-dog","fill-doghouse","fill-dryalfalfa-windrow","fill-dryclover-windrow","fill-drygrass-windrow","fill-drymaize2","fill-duck","fill-easel","fill-egg","fill-electriccharge","fill-emptypallet","fill-ethanol","fill-fabric","fill-fertilizer","fill-fishfeed","fill-fishflour","fill-floortiles","fill-flour","fill-forage","fill-forage-mixing","fill-frenchfries","fill-furniture","fill-goat","fill-goatcheese","fill-goatmilk","fill-grape","fill-grapejuice","fill-grass","fill-grass-windrow","fill-greensalad","fill-haypellets","fill-herbicide","fill-honey","fill-horse","fill-ironore","fill-lettuce","fill-lime","fill-liquidfertilizer","fill-liquidmanure","fill-maize","fill-manure","fill-mashedpotato","fill-metal","fill-methane","fill-milk","fill-mineralfeed","fill-mohn","fill-molasses","fill-oat","fill-oatdrink","fill-oatmeal","fill-oatmilk","fill-oilradish","fill-oil-canola","fill-oil-olive","fill-oil-sunflower","fill-olive","fill-onion","fill-onionjuice","fill-paperroll","fill-parsnip","fill-pasta","fill-peppergrinder","fill-pickle-carrot","fill-pickle-parsnip","fill-pickle-redbeet","fill-pictureframe","fill-pig","fill-pigfood","fill-pike","fill-pizza","fill-plankslong","fill-pomace","fill-poplar","fill-poppyseedbun","fill-potato","fill-potatopancake","fill-potatosalad","fill-praline","fill-prefabwall","fill-pressurewasheradditive","fill-product","fill-propane","fill-raisins","fill-redbeet","fill-redcabbage","fill-roadsalt","fill-rockpowder","fill-roundbalecotton","fill-roundbalegrass","fill-roundbalehay","fill-roundbalesilage","fill-roundbalestraw","fill-roundbalewood","fill-rye","fill-salmon","fill-salt","fill-seeds","fill-seedtreatingliquid","fill-sheep","fill-shingle","fill-silage","fill-silageadditive","fill-snow","fill-sodiumchloride","fill-sorghum","fill-soup-carrot","fill-soup-parsnip","fill-soup-potato","fill-soup-redbeet","fill-soup-triple","fill-soybean","fill-soydrink","fill-soyflour","fill-soymilk","fill-spelt","fill-squarebalecotton","fill-squarebalegrass","fill-squarebalehay","fill-squarebalesilage","fill-squarebalestraw","fill-squarebalewood","fill-staircaserailing","fill-stone","fill-straw","fill-strawberry","fill-strawberrycreamcake","fill-strawberryicecream","fill-strawberryjuice","fill-strawpellets","fill-sugar","fill-sugarbeet","fill-sugarbeetcut","fill-sugarcane","fill-sun","fill-sunflower","fill-table","fill-tarp","fill-tomato","fill-tomatojuice","fill-tomatosalad","fill-unknown","fill-vinegar","fill-vitamins","fill-water","fill-weed","fill-wheat","fill-wheatsemolina","fill-whiskey","fill-whitecabbage","fill-wine","fill-wood","fill-woodbeam","fill-woodchips","fill-wool","look-beacons","look-diesel","look-electric","look-engine","look-fillunit","look-income","look-info","look-key","look-lights","look-methane","look-objects","look-paintable","look-price","look-prod-cycle","look-prod-input","look-prod-output","look-speed","look-speedlimit","look-timer","look-transmission","look-weight","look-wheels","look-width","look-year","ma-large","ma-small","season-fall","season-spring","season-summer","season-winter","sort-down","sort-none","sort-up","ver-13","ver-15","ver-17","ver-19","ver-22","weather-1","weather-2","weather-3","weather-4","weather-5","weather-6","weather-7","weather-8","weather-9"])
 /* eslint-enable comma-spacing, quotes */
 
 const ft_map = {
 	'barley-flour'                  : 'flour',
 	'canola-oil'                    : 'oil-canola',
 	'canolaoil'                     : 'oil-canola',
-	'cardboard'                     : 'palletroll',
+	'cardboard'                     : 'cartonroll',
 	'carton-roll'                   : 'cartonroll',
 	'cereal'                        : 'cereals',
 	'chaff-silage'                  : 'silage',
@@ -37,6 +37,8 @@ const ft_map = {
 	'factory-power'                 : 'electriccharge',
 	'fermenter-slurry'              : 'liquidmanure',
 	'firtree'                       : 'wood',
+	'fish-feed'                     : 'fishfeed',
+	'fish-flour'                    : 'fishflour',
 	'foragemix'                     : 'forage-mixing',
 	'grape-juice'                   : 'grapejuice',
 	'grapes'                        : 'grape',
@@ -69,6 +71,7 @@ const ft_map = {
 	'pigfood-mixer'                 : 'pigfood',
 	'planks'                        : 'boards',
 	'poplartree'                    : 'poplar',
+	'poppy'                         : 'mohn',
 	'potatochips'                   : 'chips',
 	'potatoe'                       : 'potato',
 	'potatoes'                      : 'potato',
@@ -79,11 +82,13 @@ const ft_map = {
 	'silage-additive'               : 'silageadditive',
 	'silage-in'                     : 'silage',
 	'slurry'                        : 'liquidmanure',
+	'sodiumchlorid'                 : 'sodiumchloride',
 	'soldable-electricity'          : 'electriccharge',
 	'soldable-methane'              : 'methane',
 	'solid-fertilizer'              : 'fertilizer',
 	'solidfertilizer'               : 'fertilizer',
 	'sorghum-flour'                 : 'flour',
+	'soy-flour'                     : 'soyflour',
 	'soybeans'                      : 'soybean',
 	'soybeanstraw'                  : 'beanstraw',
 	'spaghetti'                     : 'pasta',
@@ -113,6 +118,7 @@ const ft_map = {
 	'tree-saplings'                 : 'wood',
 	'treesaplings'                  : 'wood',
 	'wheat-flour'                   : 'flour',
+	'wind'                          : 'air',
 	'wood-wood-beams'               : 'woodbeam',
 	'woodchipsroundbale'            : 'roundbalewood',
 	'woodchipssquarebale'           : 'squarebalewood',
